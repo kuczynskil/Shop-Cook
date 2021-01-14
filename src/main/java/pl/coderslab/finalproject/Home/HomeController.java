0850c6c514addfcdc08f3b673f2029f1ba10aeee
kuczynskil <l.kuczynski95@gmail.com>
2021-01-14 20:19:29 +0100

    Fix the homepage

--- src/main/java/pl/coderslab/finalproject/Home/HomeController.java
+++ src/main/java/pl/coderslab/finalproject/Home/HomeController.java
@@ -26,7 +26,7 @@ public class HomeController {
 
     @GetMapping("")
     public String homepage() {
-        return "home/home";
+        return "recipe/all";
     }
 
     @GetMapping("/register")
